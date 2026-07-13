193a36c9b98861693251e0a7f3e578238d0f2583
OMJR72 <jarquinorlando72@gmail.com>
2026-07-13 12:37:56 -0600

    Diseño del Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,29 @@
-
-
 function Login() {
   return (
     <>
-
+        <section className="tarjeta">
+            <form>
+                <label className="tituLogin">Iniciar Sesión</label>
+                <br/>
+                <br/>
+                <label className="texto">Correo Electronico</label>
+                <input className="campo" type="text" placeholder="example@correo.com" />
+                <br/>
+                <br/>
+                <br/>
+                <label className="texto">Contraseña</label>
+                <input className="campo" type="password" placeholder="•••••••••••" />
+                <label id="mostrar">
+                    <input type="checkbox"/>
+                    Mostrar contraseña
+                </label>
+                <label id="pregunta">¿Olvidaste la contraseña?</label>
+                <br/>
+                <br/>
+                <br/>
+                <button id="ingresar" type="submit">Iniciar Sesion</button>
+            </form>
+        </section>
     </>
   )
 }
